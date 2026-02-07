26db25610f87baa4f86e2b5036f2282795051cc3
Chase <Chase.Bjerke@gmail.com>
2026-02-06 19:36:02 -0800

    Overview Update

--- CitiBike_dashboard_Part2.py
+++ CitiBike_dashboard_Part2.py
@@ -78,7 +78,7 @@ if page == "Overview":
     # --------------------------------
     # INTRO 
     # --------------------------------
-    intro_left, intro_center, intro_right = st.columns([0.26, 1, 0.74])
+    intro_left, intro_center, intro_right = st.columns([0.28, 1, 0.52])
     with intro_center:
         st.markdown("""
 This dashboard provides a descriptive analysis of New York City’s Citi Bike system, enabling the business strategy team to understand current usage patterns and identify opportunities to enhance bike availability throughout the city.
